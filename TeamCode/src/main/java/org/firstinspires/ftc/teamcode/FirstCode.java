2bd23e8cd657001b6323d06be10d11e00cd425bb
Krish Patil <krishsmacbookair@krishs-air.eanes-ad.eanesisd.net>
2023-09-23 12:24:02 -0500

    End of Sep 23

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FirstCode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FirstCode.java
@@ -4,25 +4,39 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
 @TeleOp(name = "FirstController", group = "Pirhos")
 
 public class FirstCode extends LinearOpMode {
 
-private DcMotor frontLeft;
+    private DcMotor frontLeft;
     private DcMotor frontRight;
     private DcMotor backLeft;
     private DcMotor backRight;
 
+    private Servo arm;
+    public void strafe(double speed){
+        frontLeft.setPower(-speed);
+        frontRight.setPower(speed);
+        backLeft.setPower(-speed);
+        backRight.setPower(-speed);
 
-
+    }
+//    public void strafeRight(double speed){
+//        frontLeft.setPower(speed);
+//        frontRight.setPower(-speed);
+//        backLeft.setPower(speed);
+//        backRight.setPower(speed);
+//
+//    }
 
     public void forward(double speed){
         backRight.setPower(-speed);
         backLeft.setPower(-speed);
-        frontRight.setPower(speed);
-        frontLeft.setPower(-speed);
+        frontRight.setPower(-speed);
+        frontLeft.setPower(speed);
 
 
     }
@@ -40,21 +54,30 @@ private DcMotor frontLeft;
         frontRight = hardwareMap.get(DcMotor.class, "frntRT");
         backLeft = hardwareMap.get(DcMotor.class, "bckLF");
         backRight = hardwareMap.get(DcMotor.class, "bckRT");
+        arm = hardwareMap.servo.get("arm");
 
-        backRight.setDirection(DcMotorSimple.Direction.REVERSE);
-        backLeft.setDirection(DcMotorSimple.Direction.REVERSE);
-        frontRight.setDirection(DcMotorSimple.Direction.REVERSE);
-        frontLeft.setDirection(DcMotorSimple.Direction.REVERSE);
-
+        //BACK LEFT SHOULD BE NEGATIVE to go fwd
         Hardware2 robot = new Hardware2(false);
         ElapsedTime time = new ElapsedTime();
         waitForStart();
         while (opModeIsActive()) {
-            time.reset();
-            forward(gamepad1.left_stick_y);
-            setRight(-gamepad1.right_stick_x);
-            setLeft(gamepad1.right_stick_x);
 
+            backLeft.setPower(-gamepad1.left_stick_y);
+            backRight.setPower(gamepad1.left_stick_y);
+            frontLeft.setPower(gamepad1.left_stick_y);
+            frontRight.setPower(gamepad1.left_stick_y);
+
+            strafe(gamepad1.right_stick_x);
+
+if (gamepad1.a)
+{
+
+    arm.setDirection(Servo.Direction.REVERSE);
 }
+if(gamepad1.b){
+    arm.setDirection(Servo.Direction.FORWARD);
+}
+        }
+
 
     }}
