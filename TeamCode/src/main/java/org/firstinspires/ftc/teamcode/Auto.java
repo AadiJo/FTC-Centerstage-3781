86c98699d3bba0dd60565e63a3dc5badbf52c937
Prisha <prisha.arora@icloud.com>
2023-10-18 20:47:28 -0500

    commit this thingyyyyy

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto.java
@@ -10,9 +10,31 @@ import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.IMU;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
+import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.telemetry;
 
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.openftc.easyopencv.OpenCvCamera;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import org.opencv.core.Core;
+import org.opencv.core.Rect;
+import org.opencv.core.Scalar;
+import org.opencv.imgproc.Imgproc;
+import org.openftc.easyopencv.*;
+import org.opencv.core.Mat;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
+import org.opencv.core.Core;
+import org.opencv.core.Mat;
+import org.opencv.core.Rect;
+import org.opencv.core.Scalar;
+import org.opencv.imgproc.Imgproc;
+import org.openftc.easyopencv.OpenCvCamera;
+import org.openftc.easyopencv.OpenCvCameraFactory;
+import org.openftc.easyopencv.OpenCvCameraRotation;
+import org.openftc.easyopencv.OpenCvPipeline;
+import org.openftc.easyopencv.OpenCvWebcam;
 
 public class Auto  extends LinearOpMode {
     double error;
@@ -305,29 +327,141 @@ public class Auto  extends LinearOpMode {
 
     }
 
-private class LeftRightCenter {
-        BHI260IMU imu;
+    private class CameraAuto extends LinearOpMode {
+        OpenCvWebcam webcam = null;
+        double targetTicks;
+        CRServo arm;
+        Servo clawR;
+        Servo clawL;
+
         private DcMotor frontLeft;
         private DcMotor frontRight;
         private DcMotor backLeft;
         private DcMotor backRight;
+        DcMotor rightEncoder;
+        DcMotor midEncoder;
+        DcMotor leftEncoder;
+        double ticksPerRevODO = 2048;
+        double circumOfODO = 1.89;
+        public void setPowerToAllMotorsTo(double speed){
 
-        private double calcBotXSpeedNew(double stickX, double stickY, double botHeading) {
-            return (stickX * Math.cos(-botHeading) - stickY * Math.sin(-botHeading));
+            backLeft.setPower(speed);
+            backRight.setPower(speed);
+            frontRight.setPower(speed);
+            frontLeft.setPower(speed);
         }
+        public double getTargetTicks(double inches){
+            return (inches / circumOfODO)*ticksPerRevODO/2;}
+        @Override
+        public void runOpMode() throws InterruptedException {
+            ElapsedTime time = new ElapsedTime();
 
-        private double calcBotYSpeedNew(double stickX, double stickY, double botHeading) {
+            //WebcamName webcamName = hardwareMap.get(WebcamName.class, "Webcam_1");
+            int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("CameraMonitorViewID", "id", hardwareMap.appContext.getPackageName());
+            webcam = OpenCvCameraFactory.getInstance().createWebcam(hardwareMap.get(WebcamName.class, "Webcam_1"), cameraMonitorViewId);
 
-            return (stickX * Math.sin(-botHeading) + stickY * Math.cos(-botHeading));
+            backLeft = hardwareMap.get(DcMotor.class, "bckLF");
+            backRight = hardwareMap.get(DcMotor.class, "bckRT");
+            frontLeft = hardwareMap.get(DcMotor.class, "frntLF");
+            frontRight = hardwareMap.get(DcMotor.class, "frntRT");
+            leftEncoder = hardwareMap.get(DcMotor.class, "leftEncoder");
+            rightEncoder = hardwareMap.get(DcMotor.class, "rightEncoder");
+            midEncoder = hardwareMap.get(DcMotor.class, "frontEncoder");
+            arm = hardwareMap.crservo.get("arm");
+            clawR = hardwareMap.servo.get("clawR");
+            clawL = hardwareMap.servo.get("clawL");
+            backRight.setDirection(DcMotorSimple.Direction.FORWARD);
+            frontRight.setDirection(DcMotorSimple.Direction.FORWARD);
+            frontLeft.setDirection(DcMotorSimple.Direction.FORWARD);
+            backLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+
+            leftEncoder.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            midEncoder.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            rightEncoder.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+//        webcam.setViewportRenderingPolicy(OpenCvCamera.ViewportRenderingPolicy.OPTIMIZE_VIEW);
+            examplePipeline pipeline = new examplePipeline();
+
+            webcam.setPipeline(pipeline);
+            webcam.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener() {
+                @Override
+                public void onOpened() {
+                    webcam.startStreaming(640,360,OpenCvCameraRotation.UPRIGHT);
+                }
+                public void onError(int n){
+
+                }
+
+
+            });
+            clawR.setPosition(1);
+            clawL.setPosition(0);
+            telemetry.addLine("left "+pipeline.leftavgfinal);
+            telemetry.addLine("right "+pipeline.rightavgfinal);
+            telemetry.addLine("middle"+pipeline.midavgfinal);
+            telemetry.update();
+            waitForStart();
+            time.reset();
+            while (time.seconds() < 2){
+                setPowerToAllMotorsTo(.2);
+            }
+            setPowerToAllMotorsTo(0);
+
+            leftEncoder.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);//left movement is positive
+            midEncoder.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+            rightEncoder.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+
+            if(pipeline.position == 1) {
+                telemetry.addLine("left");
+                telemetry.update();}
+
+            else if (pipeline.position == 3){
+                telemetry.addLine("Right");
+                telemetry.update();
+            }
+
+            else if(pipeline.position == 2) {
+                telemetry.addLine("middle");
+                telemetry.update();
+                time.reset();
+                while(time.seconds()<3){
+                    setPowerToAllMotorsTo(.4);
+                }
+                time.reset();
+                while(time.seconds()<2){
+                    setPowerToAllMotorsTo(-.4);
+                }
+                setPowerToAllMotorsTo(0);
+                time.reset();
+                while(time.seconds() < 2.8){
+                    arm.setPower(1);
+                }
+                arm.setPower(0);
+                clawR.setPosition(0);
+                clawL.setPosition(1);
+
+
+            }
+            telemetry.addLine(""+pipeline.leftavgfinal);
+            telemetry.addLine(""+pipeline.rightavgfinal);
+            telemetry.addLine(""+pipeline.midavgfinal);
+
+            telemetry.update();
+
+            time.reset();
+            while (!gamepad1.a){}
+
+
+
+
+
+
 
         }
-
-        private void moveBot(double xSpeed, double ySpeed) {
-            frontLeft.setPower(ySpeed + xSpeed);
-            frontRight.setPower(ySpeed - xSpeed);
-            backLeft.setPower(ySpeed - xSpeed);
-            backRight.setPower(ySpeed + xSpeed);
-        }
-
     }
-}
+//
+//private class LeftRightCenter {
+//        if
+//    }
+//}
