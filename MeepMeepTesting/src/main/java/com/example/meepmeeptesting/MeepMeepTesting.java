b322e4c7c156f53a4d1b60c1c6bde00732d3698a
Aadi Johari <zakethegr8@gmail.com>
2023-10-20 22:42:40 -0500

    Finalize AutoTest without testing

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -6,6 +6,12 @@ import com.noahbres.meepmeep.MeepMeep;
 import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
 import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
 
+import java.awt.Image;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
 public class MeepMeepTesting {
     public static void main(String[] args) {
         MeepMeep meepMeep = new MeepMeep(800);
@@ -13,14 +19,22 @@ public class MeepMeepTesting {
         RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
                 // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
                 .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
+                .setDimensions(16, 18)
                 .followTrajectorySequence(drive ->
-                        drive.trajectorySequenceBuilder(new Pose2d(0, 0, 0))
-                                .splineTo(new Vector2d(15, 15), Math.PI / 2)
-                                .splineTo(new Vector2d(30, 0), Math.PI)
+                        drive.trajectorySequenceBuilder(new Pose2d(-71.90, -36.75, 0))
+                                .splineTo(new Vector2d(-34.79, -36.95), Math.toRadians(1.18))
+                                .splineTo(new Vector2d(-31.28, -35.61), Math.toRadians(270.00))
+                                .splineTo(new Vector2d(-49.63, -36.23), Math.toRadians(180.00))
+                                .splineTo(new Vector2d(-45.41, -57.98), Math.toRadians(-42.66))
+                                .splineTo(new Vector2d(-33.86, -47.46), Math.toRadians(270.00))
+                                .splineTo(new Vector2d(-36.64, 48.50), Math.toRadians(90.00))
                                 .build());
 
-
-        meepMeep.setBackground(MeepMeep.Background.FIELD_CENTERSTAGE_OFFICIAL)
+        Image img = null;
+        try { img = ImageIO.read(new File("C:\\Users\\AadiJ\\Projects\\FTC\\roadrunner-updated-blank\\MeepMeepTesting\\src\\main\\java\\com\\example\\meepmeeptesting\\centerstage_bg.png")); }
+        catch (IOException ignored) {}
+        assert img != null;
+        meepMeep.setBackground(img)
                 .setDarkMode(true)
                 .setBackgroundAlpha(0.95f)
                 .addEntity(myBot)
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoTest.java
@@ -4,39 +4,215 @@ import com.acmerobotics.roadrunner.Pose2d;
 import com.acmerobotics.roadrunner.Vector2d;
 import com.acmerobotics.roadrunner.ftc.Actions;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.openftc.easyopencv.OpenCvCamera;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.opencv.core.Core;
+import org.opencv.core.Rect;
+import org.opencv.core.Scalar;
+import org.opencv.imgproc.Imgproc;
+import org.openftc.easyopencv.*;
+import org.opencv.core.Mat;
+import com.qualcomm.hardware.bosch.BHI260IMU;
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.IMU;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
+import org.openftc.easyopencv.OpenCvCamera;
+import org.openftc.easyopencv.OpenCvCameraFactory;
+import org.openftc.easyopencv.OpenCvCameraRotation;
+import org.openftc.easyopencv.OpenCvWebcam;
+
 
 import org.firstinspires.ftc.teamcode.MecanumDrive;
 import org.firstinspires.ftc.teamcode.TankDrive;
 
+
+
 public final class AutoTest extends LinearOpMode {
+
+    OpenCvWebcam webcam = null;
+    double targetTicks;
+    CRServo arm;
+    Servo clawR;
+    Servo clawL;
+    private BHI260IMU imu;
+    Orientation angles = new Orientation();
+
+    class examplePipeline extends OpenCvPipeline {
+
+        byte position;
+        Mat YCbCr = new Mat();
+        Mat leftCrop;
+        Mat rightCrop;
+        Mat midCrop;
+        double leftavgfinal;
+        double rightavgfinal;
+        double midavgfinal;
+
+        Mat output = new Mat();
+        Scalar rectColor =  new Scalar(225,0,0); //225, 0, 0
+//    OpenCvPipeline examplePipeline;
+
+        @Override
+        public Mat processFrame(Mat input) {
+
+            Imgproc.cvtColor(input, YCbCr, Imgproc.COLOR_RGB2YCrCb);
+
+
+            Rect leftRect = new Rect(1,1,213,359);
+            Rect rightRect = new Rect(426,1, 213, 359);
+            Rect middleRect = new Rect(213,179,213,180);
+
+            input.copyTo(output);
+            Imgproc.rectangle(output,leftRect,rectColor,4);
+            Imgproc.rectangle(output,rightRect,rectColor,4);
+            Imgproc.rectangle(output,middleRect,rectColor,4);
+
+            leftCrop = YCbCr.submat(leftRect);
+            rightCrop = YCbCr.submat(rightRect);
+            midCrop =YCbCr.submat(middleRect);
+
+            Core.extractChannel(leftCrop, leftCrop,2);
+            Core.extractChannel(rightCrop, rightCrop,2);
+            Core.extractChannel(midCrop,midCrop,2);
+
+            Scalar leftavg = Core.mean(leftCrop);
+            Scalar rightavg = Core.mean(rightCrop);
+            Scalar midavg = Core.mean(midCrop);
+
+            leftavgfinal= leftavg.val[0];
+            rightavgfinal= rightavg.val[0];
+            midavgfinal = midavg.val[0];
+
+            if (2*(leftavgfinal)<rightavgfinal +midavgfinal){
+                position = 1;
+
+            }
+            else if (2*(midavgfinal)<leftavgfinal+rightavgfinal){
+                position = 2;
+
+            }
+            else if(rightavgfinal*2<leftavgfinal+rightavgfinal){ position = 3;}
+
+
+            return output;
+
+        }
+
+    }
+
+
     @Override
     public void runOpMode() throws InterruptedException {
-        MecanumDrive drive = new MecanumDrive(hardwareMap, new Pose2d(-69.22, 4.9, Math.toRadians(2.49)));
+
+        ElapsedTime time = new ElapsedTime();
+
+        MecanumDrive drive = new MecanumDrive(hardwareMap, new Pose2d(-71.90, -36.75, Math.toRadians(0.00)));
+
+        int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("CameraMonitorViewID", "id", hardwareMap.appContext.getPackageName());
+        webcam = OpenCvCameraFactory.getInstance().createWebcam(hardwareMap.get(WebcamName.class, "Webcam_1"), cameraMonitorViewId);
+
+        examplePipeline pipeline = new examplePipeline();
+
+        webcam.setPipeline(pipeline);
+        webcam.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener() {
+            @Override
+            public void onOpened() {
+                webcam.startStreaming(640, 360, OpenCvCameraRotation.UPRIGHT);
+            }
+
+            public void onError(int n) {
+
+            }
+
+
+        });
+
+        telemetry.addLine("left " + pipeline.leftavgfinal);
+        telemetry.addLine("right " + pipeline.rightavgfinal);
+        telemetry.addLine("middle" + pipeline.midavgfinal);
+        telemetry.update();
+
+
+
 
         waitForStart();
 
+        time.reset();
+
         Actions.runBlocking(
                 drive.actionBuilder(drive.pose)
-                        .splineTo(new Vector2d(-64.99, 5.41), Math.toRadians(6.95))
-                        .splineTo(new Vector2d(-63.14, -10.77), Math.toRadians(270.00))
-                        .splineTo(new Vector2d(-61.18, -22.32), Math.toRadians(-74.05))
-                        .splineTo(new Vector2d(-59.42, -34.07), Math.toRadians(-80.54))
-                        .splineTo(new Vector2d(-47.26, -49.32), Math.toRadians(-51.43))
-                        .splineTo(new Vector2d(-38.71, -54.27), Math.toRadians(-87.73))
-                        .splineTo(new Vector2d(-38.81, -61.90), Math.toRadians(269.23))
-                        .splineTo(new Vector2d(-30.67, -62.41), Math.toRadians(45.00))
-                        .splineTo(new Vector2d(-21.08, -53.03), Math.toRadians(83.75))
-                        .splineTo(new Vector2d(-21.08, -42.11), Math.toRadians(126.87))
-                        .splineTo(new Vector2d(-31.70, -33.24), Math.toRadians(144.46))
-                        .splineTo(new Vector2d(-37.47, -16.23), Math.toRadians(80.13))
-                        .splineTo(new Vector2d(-37.06, -13.86), Math.toRadians(90.00))
-                        .splineTo(new Vector2d(-37.06, -3.04), Math.toRadians(84.91))
-                        .splineTo(new Vector2d(-35.92, 12.01), Math.toRadians(92.94))
-                        .splineTo(new Vector2d(-36.85, 32.83), Math.toRadians(87.77))
-                        .splineTo(new Vector2d(-37.16, 48.50), Math.toRadians(88.67))
+                        .splineTo(new Vector2d(-34.79, -36.95), Math.toRadians(1.18))
+                        .build()
+        );
+
+        if (pipeline.position == 1){
+            // LEFT
+            telemetry.addLine("left");
+            telemetry.update();
+
+            Actions.runBlocking(
+                    drive.actionBuilder(drive.pose)
+                            .splineTo(new Vector2d(-31.28, -35.61), Math.toRadians(90.00))
+                            .build()
+            );
+
+
+
+
+        }
+
+        else if (pipeline.position == 3){
+            // RIGHT
+            telemetry.addLine("Right");
+            telemetry.update();
+
+            Actions.runBlocking(
+                    drive.actionBuilder(drive.pose)
+                            .splineTo(new Vector2d(-31.28, -35.61), Math.toRadians(270.00))
+                            .build()
+            );
+
+        }
+
+        else if (pipeline.position == 2){
+            // MIDDLE
+            telemetry.addLine("middle");
+            telemetry.update();
+
+        }
+
+        sleep(1500);
+
+        Actions.runBlocking(
+                drive.actionBuilder(drive.pose)
+                        .splineTo(new Vector2d(-49.63, -36.23), Math.toRadians(180.00))
+                        .splineTo(new Vector2d(-45.41, -57.98), Math.toRadians(-42.66))
+                        .splineTo(new Vector2d(-33.86, -47.46), Math.toRadians(270.00))
+
                         .build());
 
+        sleep(1500);
 
+        Actions.runBlocking(
+                drive.actionBuilder(drive.pose)
+                        .splineTo(new Vector2d(-36.64, 48.50), Math.toRadians(90.00))
+                        .build());
 
     }
 }
+
+
