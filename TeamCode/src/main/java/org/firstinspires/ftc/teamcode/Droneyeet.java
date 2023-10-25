70d837c8df535bb1c45bb010ebc90d2f6c238e42
Seth the cat <tyicetea13@gmail.com>
2023-10-25 00:39:07 +0000

    does this work

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Droneyeet.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Droneyeet.java
@@ -1,1 +1,20 @@
+package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.hardware.bosch.BHI260IMU;
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.IMU;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
+
+public class Droneyeet {
+    DcMotor yeetmotor = yeet.motor;
+    yeetmotor.set(0.5)
+}
