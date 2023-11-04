e252aa5dc04876c777886c348de9de18f8213e3a
Seth-the-cat <tyicetea13@gmail.com>
2023-11-04 11:57:15 -0500

    it no wokrs

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Droneyeet.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Droneyeet.java
@@ -1,32 +1,28 @@
 package org.firstinspires.ftc.teamcode;
 
+import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.gamepad1;
+import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.hardwareMap;
+
+import com.qualcomm.hardware.bosch.BHI260IMU;
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.IMU;
 import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
 public class Droneyeet {
-    yeet.motor = hardwareMap.get(DcMotor.class, "yeetmotor");
-    boosh = hardwareMap.get(DcMotor.class, "boosh");
-    while( gamepad1.a){
+    private Servo boosh = new Servo();
+    private yeetmotor = yeetmotor;
+    yeetmotor = hardwareMap.get(DcMotor.class, "yeetmotor");
+    boosh = hardwareMap.get(Servo.class, "boosh");
+    while (gamepad1.a) {
         yeetmotor.setPower(1);
-        Servo boosh = boosh;
         boosh.setPosition(0);
         boosh.setPosition(1);
-        motor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        yeetmotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
     }
 }
-
-
-//initialize yeet_motor()√
-//Start_launch_motor(rpm)√
-//initialize servo()√
-// set_servo_limits (min degree, max degree)√
-//push_drone(){
-//    move_servo(max)
-//}√
-//Stop_servo(){
-    //move_servo(min) 
-// }√
-////stop_launch_motor()√
-    
