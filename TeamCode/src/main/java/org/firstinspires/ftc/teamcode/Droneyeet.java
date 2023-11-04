39d5672613c200c442900d1bd352856333aa931e
PRISHA ARORA <pa98703@eanesisd.net>
2023-11-04 00:46:46 +0000

    wejustneedtotest

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Droneyeet.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Droneyeet.java
@@ -11,27 +11,30 @@ import com.qualcomm.robotcore.hardware.IMU;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import java.lang.Math;
-
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
 
 public class Droneyeet {
-    DcMotor yeetmotor = yeet.motor;
+    yeet.motor = hardwareMap.get(DcMotor.class, "yeetmotor");
+    while( gamepad1.a){
     yeetmotor.setPower(1);
     Servo boosh = boosh;
     boosh.setPosition(0);
-    boosh.setPosition(0.5);
-    exit();
-} 
-//initialize launch_motor()
-//Start_launch_motor(rpm)
-//initialize servo ()
-// set_servo_limits (min degree, max degree)
+    boosh.setPosition(1);
+    yeetmotor.setPower(0);
+}
+}
+
+
+//initialize yeet_motor()√
+//Start_launch_motor(rpm)√
+//initialize servo()√
+// set_servo_limits (min degree, max degree)√
 //push_drone(){
 //    move_servo(max)
-//}
+//}√
 //Stop_servo(){
     //move_servo(min) 
-// }
-////stop_launch_motor()
+// }√
+////stop_launch_motor()√
     
