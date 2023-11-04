118e82ea4f35f70b5fb8f23204d43945d1e156e0
Seth the cat <tyicetea13@gmail.com>
2023-11-04 15:04:23 +0000

    Update file Droneyeet.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Droneyeet.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Droneyeet.java
@@ -1,18 +1,9 @@
 package org.firstinspires.ftc.teamcode;
 
-import com.qualcomm.hardware.bosch.BHI260IMU;
-import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
-import com.qualcomm.robotcore.hardware.IMU;
 import com.qualcomm.robotcore.hardware.Servo;
-import com.qualcomm.robotcore.util.ElapsedTime;
-import java.lang.Math;
-import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
-import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
 
 public class Droneyeet {
     yeet.motor = hardwareMap.get(DcMotor.class, "yeetmotor");
